cd54645f7cadccfb2201a766572987072d82eaf4
Ashley's PC <ashlynn4567@gmail.com>
2022-05-30 23:20:05 -0500

    added employee and engineer tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,58 @@
 // imports
 const Employee = require("../lib/Employee");
 
-//
+// tests
+test("Ensures that employee is created as an object", () => {
+    const employee = new Employee();
+
+    expect(typeof employee).toBe("object");
+});
+
+test("Verifies name is created correctly in Employee constructor", () => {
+    const name = "Ashley";
+    const employee = new Employee(name);
+
+    expect(employee.name).toBe(name);
+});
+
+test("Verifies ID is created correctly in Employee constructor", () => {
+    const id = 1;
+    const employee = new Employee("Ashley", id);
+
+    expect(employee.id).toBe(id);
+});
+
+test("Verifies email is created correctly in Employee constructor", () => {
+    const email = "ashley@yahoo.com";
+    const employee = new Employee("Ashley", 1, email);
+
+    expect(employee.email).toBe(email);
+});
+
+test("Ensures employee.getName() returns employee's name", () => {
+    const name = "Ashley";
+    const employee = new Employee(name);
+
+    expect(employee.getName()).toBe(name);
+});
+
+test("Ensures employee.getId() returns employee's id", () => {
+    const id = 1;
+    const employee = new Employee("Ashley", id);
+
+    expect(employee.getId()).toBe(id);
+});
+
+test("Ensures employee.getEmail() returns employee's email", () => {
+    const email = "ashley@yahoo.com";
+    const employee = new Employee("Ashley", 1, email);
+
+    expect(employee.getEmail()).toBe(email);
+});
+
+test("Ensures employee.getRole() returns employee's role", () => {
+    const role = "Employee";
+    const employee = new Employee("Ashley", 1, "ashley@yahoo.com");
+
+    expect(employee.getRole()).toBe(role);
+});
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,25 @@
 // imports 
 const Engineer = require("../lib/Engineer");
 
-//
+// tests
+test("Ensures github username is created correctly in Engineer Constructor", () => {
+    const github = 'ashlynn4567';
+    // placeholder values for name, id, and email
+    const engineer = new Engineer("Ashley", 1, "ashley@yahoo.com", github);
+
+    expect(engineer.github).toBe(github);
+});
+
+test("Ensures engineer.getGithub() returns github username", () => {
+    const github = 'ashlynn4567'
+    const engineer = new Engineer("Ashley", 1, "ashley@yahoo.com", github);
+
+    expect(engineer.getGithub()).toBe(github);
+});
+
+test("Ensures engineer.getRole() returns engineer's role", () => {
+    const role = 'Engineer';
+    const engineer = new Engineer("Ashley", 1, "ashley@yahoo.com", 'ashlynn4567');
+
+    expect(engineer.getRole()).toBe(role);
+});
