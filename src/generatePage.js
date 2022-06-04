82b33f0c4d1f18fbbef56d70c86d4ea979be4034
Ashley's PC <ashlynn4567@gmail.com>
2022-06-04 17:52:44 -0500

    bug fixes

--- src/generatePage.js
+++ src/generatePage.js
@@ -82,36 +82,36 @@ const assembleCards = teamArr => {
 // putting together the whole template
 const generatePage = team => {
   return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Team Builder</title>
-        <link href="./stylesheets/style.css" rel="stylesheet" />
-        <!-- Google Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-        href="https://fonts.googleapis.com/css2?family=Lora&family=Roboto:wght@700&display=swap"
-        rel="stylesheet"
-        />
-        <!-- Font Awesome Icons -->
-        <script
-        src="https://kit.fontawesome.com/c67bd88601.js"
-        crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <div class="bg-primary">
-        <h1>Team Builder</h1>
-        </div>
-        <div class="card-container">
-            ${assembleCards(team)}
-        </div>
-    </body>
-    </html>
-    `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team Builder</title>
+    <link href="./stylesheets/style.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+    href="https://fonts.googleapis.com/css2?family=Lora&family=Roboto:wght@700&display=swap"
+    rel="stylesheet"
+    />
+    <!-- Font Awesome Icons -->
+    <script
+    src="https://kit.fontawesome.com/c67bd88601.js"
+    crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="bg-primary">
+    <h1>Team Builder</h1>
+    </div>
+    <div class="card-container">
+        ${assembleCards(team)}
+    </div>
+</body>
+</html>
+`
 };
 
 // exports
